6f32651dab908dbb01b8fb8992aea0510d6f8402
Josemaria-De-Alba <142268975+Josemaria-De-Alba@users.noreply.github.com>
2024-11-23 12:43:36 -0600

    Add files via upload

---
+++ 9_Hash_A01706847/quadratic.h
@@ -0,0 +1,140 @@
+/*
+
+Josemaria De Alba
+A01706847
+
+*/
+
+#ifndef QUADRATIC_H
+#define QUADRATIC_H
+
+#include <string>
+#include <sstream>
+
+template <class Key, class Value>
+class Quadratic {
+private:
+	unsigned int (*func) (const Key);
+	unsigned int size;
+	unsigned int count;
+
+	Key *keys;
+	Key initialValue;
+	Value *values;
+
+	long indexOf(const Key) const;
+
+public:
+	Quadratic(unsigned int, Key, unsigned int (*f) (const Key)) ;
+	~Quadratic();
+	bool full() const;
+	bool put(Key, Value);
+	Value get(const Key) ;
+	std::string toString() const;
+};
+
+template <class Key, class Value>
+Quadratic<Key, Value>::Quadratic(unsigned int sze, Key init, unsigned int (*f) (const Key))  {
+	size = sze;
+	keys = new Key[size];
+    if (keys == 0) {
+		delete keys;
+	}
+	initialValue = init;
+	for (unsigned int i = 0; i < size; i++) {
+		keys[i] = init;
+	}
+	values = new Value[size];
+	for (int i = 0; i  < sze; i++){
+        values[i] = 0;
+    }
+
+	func = f;
+	count = 0;
+}
+
+template <class Key, class Value>
+Quadratic<Key, Value>::~Quadratic() {
+	delete [] keys;
+	keys = 0;
+	delete [] values;
+	values = 0;
+	size = 0;
+	func = 0;
+	count = 0;
+
+}
+
+template <class Key, class Value>
+bool Quadratic<Key, Value>::full() const {
+	return (count > size);
+}
+
+template <class Key, class Value>
+long Quadratic<Key, Value>::indexOf(const Key k) const {
+	unsigned int i, start;
+
+	start = i = func(k) % size;
+	do {
+		if (keys[i] == k) {
+			return i;
+		}
+		i = (i + 1) % size;
+	} while (start != i);
+	return -1;
+}
+
+template <class Key, class Value>
+bool Quadratic<Key, Value>::put(Key k, Value v) {
+    if (full()) return false;
+
+    unsigned int start, j = 0;
+    long pos;
+
+    pos = indexOf(k);
+    if (pos != -1) {
+        values[pos] = v;
+        return true;
+    }
+
+    start = func(k) % size;
+    unsigned int i = start;
+    do {
+        i = (start + j * j) % size;
+        if (keys[i] == initialValue) {
+            keys[i] = k;
+            values[i] = v;
+            count++;
+            return true;
+        }
+        j++;
+    } while (j < size);
+    return false;
+}
+
+template <class Key, class Value>
+Value Quadratic<Key, Value>::get(const Key k)  {
+	long pos;
+
+	pos = indexOf(k);
+	if (pos != -1) {
+		return values[pos];
+	}
+    return 0;
+}
+
+
+template <class Key, class Value>
+std::string Quadratic<Key, Value>::toString() const {
+	std::stringstream aux;
+
+	for (unsigned int i = 0; i < size; i++) {
+        if (keys[i] != initialValue) {
+            aux << "(" << i << " " << keys[i] << " : " << values[i] << ") ";
+        }
+    }
+	return aux.str();
+}
+
+
+#endif
